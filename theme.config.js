e7653cef9f8c1ef2e5cb3206a3e94271365b07d5
forecho <caizhenghai@gmail.com>
2021-10-18 20:22:54 +0800

    fix meta data

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,12 @@
 export default {
-  github: 'https://github.com/shuding/nextra',
-  docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  github: 'https://github.com/cashwarden',
+  docsRepositoryBase: 'https://github.com/cashwarden/guide',
+  titleSuffix: ' – CashWarden',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">CashWarden</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        CashWarden 使用指南
       </span>
     </>
   ),
@@ -16,45 +16,43 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="CashWarden: 一款开源的资产管理系统" />
+      <meta
+        name="og:description"
+        content="CashWarden: 一款开源的资产管理系统"
+      />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta
+        name="twitter:image"
+        content="https://blog-1251237404.cos.ap-guangzhou.myqcloud.com/20211018mFoYxP.png"
+      />
+      <meta name="twitter:site:domain" content="cashwarden.com" />
+      <meta name="twitter:url" content="https://cashwarden.com/" />
+      <meta name="og:title" content="CashWarden: 一款开源的资产管理系统" />
+      <meta
+        name="og:image"
+        content="https://blog-1251237404.cos.ap-guangzhou.myqcloud.com/20211018mFoYxP.png"
+      />
+      <meta name="apple-mobile-web-app-title" content="CashWarden" />
+      <link rel="apple-touch-icon" sizes="152x152" href="/favicon.png" />
       <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-icon-180x180.png"
+        rel="icon"
+        type="image/png"
+        sizes="120x120"
+        href="/favicon-120x120.png"
       />
       <link
         rel="icon"
         type="image/png"
-        sizes="192x192"
-        href="/android-icon-192x192.png"
+        sizes="60x60"
+        href="/favicon-60x60.png"
       />
       <link
         rel="icon"
         type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
+        sizes="76x76"
+        href="/favicon-76x76.png"
       />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="96x96"
-        href="/favicon-96x96.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
     </>
   ),
   search: true,
